422b3de8a011f1b6d91a03ff93a5bd80e172a1f0
tatsuya828828 <kkjy011894@gmail.com>
2020-11-03 19:42:35 +0900

    リストに表示するデータとレイアウトマネージャーの設定を追加

--- app/src/main/java/com/kuma/sampleapp3/MainActivity.java
+++ app/src/main/java/com/kuma/sampleapp3/MainActivity.java
@@ -1,8 +1,14 @@
 package com.kuma.sampleapp3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // リストに表示するデータを準備
+        String[] titles = {"pagan", "Silence Speaks", "DOWN", "Player Position", "蛍"};
+        String[] tags = {"Vitalism", "While She Sleeps", "Destine", "Periphery", "THE BACK HORN"};
+        String[] descs = {"ブラジルのバンドです", "イギリスのバンドです", "オランダのバンドです",
+                        "アメリカのバンドです", "日本のバンドです"};
+        // 配列の内容をListItemオブジェクトに詰め替え
+        ArrayList<ListItem> data = new ArrayList<>();
+        for(int i = 0; i < titles.length; i++) {
+            ListItem item = new ListItem();
+            item.setId((new Random()).nextLong());
+            item.setTitle(titles[i]);
+            item.setTag(tags[i]);
+            item.setDesc(descs[i]);
+            data.add(item);
+        }
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        // 固定サイズの場合にパフォーマンスを向上
+        // setHasFixedSizeはRecyclerViewのサイズが固定であることを宣言する
+        // あらかじめ宣言しておくことでパフォーマンスが向上する
+        recyclerView.setHasFixedSize(true);
+        // レイアウトマネージャーの準備&設定
+        // レイアウトを制御するためのレイアウトマネージャーを準備
+        // LinearLayoutManagerオブジェクトは、縦並び/横並びのリストを生成するためのレイアウトマネージャー
+        // 最低限、setOrientationでリストのスクロール方向を決定しておく必要がある
+        // VERTICALで縦、HORIZONTALで横にスクロールする
+        // 生成したレイアウトマネージャーは、RecyclerManager.setLayoutManagerでRecyclerViewに設定できる
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        manager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(manager);
+        // アダプターをRecyclerManagerに設定
+        // アダプター(今回は、MyListAdapter)を生成し、setAdapterでRecyclerViewに割り当てる
+        RecyclerView.Adapter adapter = new MyListAdapter(data);
+        recyclerView.setAdapter(adapter);
     }
 }
