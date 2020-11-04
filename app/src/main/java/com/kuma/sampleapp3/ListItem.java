1ece75a8a38d3ea80e66225be89a131f57b72f31
tatsuya828828 <kkjy011894@gmail.com>
2020-11-04 11:09:49 +0900

    フィールド名変更、setter、getter名変更

--- app/src/main/java/com/kuma/sampleapp3/ListItem.java
+++ app/src/main/java/com/kuma/sampleapp3/ListItem.java
@@ -3,16 +3,16 @@ package com.kuma.sampleapp3;
 class ListItem {
     private long id = 0;
     private String title = null;
-    private String tag = null;
-    private String desc = null;
+    private String country = null;
+    private String name = null;
 
     long getId() {return id;}
     String getTitle() {return title;}
-    String getTag() {return tag;}
-    String getDesc() {return desc;}
+    String getCountry() {return country;}
+    String getName() {return name;}
 
     void setId(long id) {this.id = id;}
     void setTitle(String title) {this.title = title;}
-    void setTag(String tag) {this.tag = tag;}
-    void setDesc(String desc) {this.desc = desc;}
+    void setCountry(String country) {this.country = country;}
+    void setName(String name) {this.name = name;}
 }
